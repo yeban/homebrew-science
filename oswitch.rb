9fbbb2071f207789923b659d5bf383d1636d2c56
Roman Valls Guimera <brainstorm@nopcode.org>
2015-11-15 16:01:05 +0100

    Corrected all jenkins bot offenses, should be clear now @yuban

--- oswitch.rb
+++ oswitch.rb
@@ -1,24 +1,23 @@
-# Reference
-# ---------
-# 1. https://github.com/Homebrew/homebrew/blob/master/Library/Contributions/example-formula.rb
-# 2. https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md#formula-cookbook
-
 class Oswitch < Formula
-	desc 'Provides a "virtual environment" around your data to work with a range of Bioinformatics tools'
-  homepage 'https://github.com/yeban/oswitch'
+  desc "Docker environments to work with Bioinformatics tools"
+  homepage "https://github.com/yeban/oswitch"
 
-  url  'https://github.com/yeban/oswitch/archive/v0.2.6.tar.gz'
-  sha256 'af3ce13305be8747242dc11bdd35360b568ea738be8c490d448915d4c675871f'
+  url "https://github.com/yeban/oswitch/archive/v0.2.6.tar.gz"
+  sha256 "af3ce13305be8747242dc11bdd35360b568ea738be8c490d448915d4c675871f"
 
   def install
     # Build gem and install to prefix.
     system "gem", "build", "oswitch.gemspec"
-		system "gem", "install", "-i", "#{prefix}", "oswitch-#{version}.gem"
+    system "gem", "install", "-i", "oswitch-#{version}.gem"
 
     # Re-write RubyGem generated bin stub to load oswitch from prefix.
     inreplace "#{bin}/oswitch" do |s|
       s.gsub!(/require 'rubygems'/,
-              "ENV['GEM_HOME']='#{prefix}'\nrequire 'rubygems'")
+        "ENV['GEM_HOME']='#{prefix}'\nrequire 'rubygems'")
     end
+  end
+
+  test do
+    print "No tests for this package"
   end
 end
